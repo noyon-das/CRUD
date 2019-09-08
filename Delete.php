e4115a36cabe4cc08573d2cb05044723d4a1baf2
noyon-das <52575249+noyon-das@users.noreply.github.com>
2019-09-08 07:54:44 +0600

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,1 +1,17 @@
 
+<?php include_once('header.php')?>
+<?php include_once('Update/conn.php');?>
+	<?php 
+		$id = $_GET['id'];
+		$sqlDel = "DELETE FROM `update` WHERE id = '$id'";
+		$fire   = mysqli_query($conn,$sqlDel);
+		if($fire){
+			header("Location: index.php");
+		}
+		else{
+			echo "Not connected";
+		}
+		/*----------------------*/
+		
+	?>
+<?php include_once('footer.php');?>
